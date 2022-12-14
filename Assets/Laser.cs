2840d592d537002b99b5b06cd13a6b07c62fad2b
jessicach4n <e.jchan@etu.cvm.qc.ca>
2022-12-14 17:06:02 -0500

    laser

---
+++ Assets/Laser.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public LayerMask masqueRaycast;
+    public float maxDistance;
+    public mouvementRadish radish;
+    private VolumetricLines.VolumetricLineBehavior laser;
+
+    void Start()
+    {
+        laser = GetComponent<VolumetricLines.VolumetricLineBehavior>();
+        laser.StartPos = new Vector3(0.0f, 0.0f, 0.0f);
+        laser.EndPos = new Vector3(0.0f, 500.0f, 0.0f);
+        maxDistance = 1000.0f;
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, maxDistance, masqueRaycast);
+
+        if (radish.nbPiesCollected == radish.maxPies)
+        {
+            laser.EndPos = laser.StartPos;
+
+        }
+        else if (hit.collider != null)
+        {
+            laser.EndPos = new Vector3(0.0f, hit.distance, 0.0f);
+
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+            {
+                hit.collider.gameObject.GetComponent<mouvementRadish>().OnHitLazer();
+            }
+        }
+        else
+        {
+            laser.EndPos = new Vector3(0.0f, 500.0f, 0.0f);
+        }
+
+
+    }
+}
--- Assets/mouvementRadish.cs
+++ Assets/mouvementRadish.cs
@@ -122,4 +122,14 @@ public class mouvementRadish : MonoBehaviour
             }
         }
     }
+
+    public void OnHitLazer()
+    {
+        Reload();
+    }
+
+    public void Reload()
+    {
+        
+    }
 }
